ab80a6a5e2badc4670ca23d71563f8124cb7e17b
Dylan Evelette <evelette.dylan@gmail.com>
2026-04-09 17:52:38 +0200

    Initial Commit

---
+++ notext/notextApp.swift
@@ -0,0 +1,17 @@
+//
+//  notextApp.swift
+//  notext
+//
+//  Created by Evelette Dylan on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct notextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
